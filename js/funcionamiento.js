08fa9579ff1911c1074c0a81fd948fccfe07ec58
Armando Beltran <beltran.armando2210@gmail.com>
2021-09-24 22:06:05 -0500

    Actividad 4 agregada

--- js/funcionamiento.js
+++ js/funcionamiento.js
@@ -24,7 +24,7 @@ function respuestas()
 
     for (var i = 0; i < 5; i++)
     {
-        var r = document.getElementById("respuestas"+rNum.toString())
+        var r = document.getElementById("respuestas"+rNum.toString());
         
         resps[i] = r.options[r.selectedIndex].text; //Toma la opción que se seleccionó, y la guarda en un arreglo de respuestas, para tenerlas almacenadas y después escribirlas
         preg[i] = document.querySelector(".preg"+rNum.toString()).textContent; //Obtener el contenido de los h5 que tienen las preguntas -- esto para ahorrar líneas de código en no ponerlas una por una. 
@@ -40,4 +40,185 @@ function respuestas()
     
     
 }
+
+document.getElementById('reservMesas').addEventListener("change", function(){ mesas();});
+document.getElementById('mostrar').addEventListener("click", function(){mostrar();});
+
+
+let mesas = function()
+{
+    let numMesas = document.getElementById("reservMesas").value; 
+    let contenido = ''; 
+    let i; 
+
+    for(i = 0; i < numMesas; i++)
+    {
+        contenido += '<h3>Mesa '+(i+1)+'</h3>'+
+        '<h4> Número de comensales </h4>'+
+        '<p>Adultos: </p>'+
+        '<select name="adultos" id="adultos'+i+'" class="form-select" onChange="numMenores();">'+
+            '<option value="0">Num. Adultos</option>'+
+            '<option value="1">1</option>'+
+            '<option value="2">2</option>'+
+            '<option value="3">3</option>'+
+            '<option value="4">4</option>'+
+        '</select><br>'+
+        '<p>Niños: </p>'+
+        '<select name="menores" id="menores'+i+'" class="form-select" onChange="edades();"> '+
+            '<option value="0">Num. Niños</option>'+
+            '<option value="1">1</option>'+
+            '<option value="2">2</option>'+
+            '<option value="3">3</option>'+
+            '<option value="4">4</option>'+
+        '</select><br>'+
+        '<div id="edades'+i+'" class="form-group"></div><hr>';
+    }
+
+    document.getElementById("mesas").innerHTML = contenido; 
+
+}
+
+let numMenores = function()
+{
+    let numMesas = document.getElementById("reservMesas").value;
+    for(let i = 0; i < numMesas; i++)
+    {
+        if(document.getElementById("adultos"+i))
+        {
+            let numAdultos = document.getElementById("adultos"+i).value; 
+            switch(numAdultos)
+            {
+                case '1': 
+                    document.getElementById("menores"+i).innerHTML = '<option value="0">Num. Niños</option>'+
+                    '<option value="1">1</option>'+
+                    '<option value="2">2</option>'+
+                    '<option value="3">3</option>'+
+                    '<option value="4">4</option>';
+                    break;
+                case '2': 
+                    document.getElementById("menores"+i).innerHTML = '<option value="0">Num. Niños</option>'+
+                    '<option value="1">1</option>'+
+                    '<option value="2">2</option>'+
+                    '<option value="3">3</option>';
+                    break;
+                case '3': 
+                    document.getElementById("menores"+i).innerHTML = '<option value="0">Num. Niños</option>'+
+                    '<option value="1">1</option>'+
+                    '<option value="2">2</option>';
+                    break; 
+                case '4': 
+                    document.getElementById("menores"+i).innerHTML = '<option value="0">0</option>';
+                    document.getElementById("edades"+i).innerHTLM = '';
+                    break;  
+                default: 
+                    break; 
+            }
+        }
+    }
+}
+
+let edades = function()
+{
+    let numMesas = document.getElementById("reservMesas").value;
+    for(let i = 0; i < numMesas; i++)
+    {
+        let contenido = '';
+        if(document.getElementById("menores"+i))
+        {
+            let numMenores = document.getElementById("menores"+i).value;
+            for(let j = 0; j < numMenores; j++)
+            {
+                contenido += '<label>Niño '+(j+1)+'<input type="text" class="form-control" name="edadMenores" id="edadMenores'+j+'"></label>'; 
+                document.getElementById("edades"+i).innerHTML = contenido; 
+            }
+        }
+    }
+}
+
+let mostrar = function()
+{
+    //Mesas
+    let numMesas = document.getElementById("reservMesas").value;
+    localStorage.setItem('numMesas', numMesas); 
+
+    //Adultos
+    let numAdultos = document.getElementsByName("adultos"); 
+    let valoresAdultos = []; 
+    for(let i = 0; i < numAdultos.length; i++)
+    {
+        let a = document.getElementById("adultos"+i); 
+        valoresAdultos[i] = a.options[a.selectedIndex].value;
+        localStorage.setItem('valoresAdultos', valoresAdultos);
+    }
+
+    //Niños
+    let numMenores = document.getElementsByName("menores"); 
+    let valoresMenores = []; 
+    for(let i = 0; i < numMenores.length; i++)
+    {
+        let n = document.getElementById("menores"+i); 
+        valoresMenores[i] = n.options[n.selectedIndex].value;
+        localStorage.setItem('valoresMenores', valoresMenores); 
+    }
+    //Edad niños
+    let edadMenores = document.getElementsByName("edadMenores"); 
+    let valoresEdad = []; 
+    for(let i = 0; i < edadMenores.length; i++)
+    {
+        valoresEdad[i] = edadMenores[i].value; 
+        localStorage.setItem('valoresEdad', valoresEdad);
+    }
+
+    window.open("reservacion.html"); 
+}
+
+
+/*'<p>Niños: </p>'+
+            '<select name="menores" id="menores'+i+'" class="form-select">'+
+                '<option value="">Num. Niños</option>'+
+                '<option value="1">1</option>'+
+                '<option value="2">2</option>'+
+                '<option value="3">3</option>'+
+                '<option value="4">4</option>'+
+            '</select><br><hr>';*/
+
+/*
+//Mesas
+    let numMesas = document.getElementById("reservMesas").value;
+    localStorage.setItem('numMesas', numMesas); 
+
+    //Adultos
+    let numAdultos = document.getElementsByName("adultos"); 
+    let valoresAdultos = []; 
+    for(let i = 0; i < numAdultos.length; i++)
+    {
+        let a = document.getElementById("adultos"+i); 
+        valoresAdultos[i] = a.options[a.selectedIndex].value;
+        localStorage.setItem('valoresAdultos', valoresAdultos);
+    }
+
+    //Niños
+    let numMenores = document.getElementsByName("menores"); 
+    let valoresMenores = []; 
+    for(let i = 0; i < numMenores.length; i++)
+    {
+        let n = document.getElementById("menores"+i); 
+        valoresMenores[i] = n.options[n.selectedIndex].value;
+        localStorage.setItem('valoresMenores', valoresMenores); 
+    }
+    //Edad niños
+    let edadMenores = document.getElementsByName("edadMenores"); 
+    let valoresEdad = []; 
+    for(let i = 0; i < edadMenores.length; i++)
+    {
+        valoresEdad[i] = edadMenores[i].value; 
+        localStorage.setItem('valoresEdad', valoresEdad);
+    }
+
+    window.open("reservacion.html"); 
+*/
+
+
+
+
 
---
+++ js/reservacionResultado.js
@@ -0,0 +1,44 @@
+(function()
+{
+    let numMesas = localStorage.getItem('numMesas'); 
+    let contenido = ''; 
+    if(numMesas > 1)
+    {
+        contenido += '<h1>Reservación de las '+numMesas+' Mesas</h1>'; 
+    } 
+    else
+    {
+        contenido += '<h1>Reservación de la mesa</h1>';
+    }
+
+    let valoresAdultos = localStorage.getItem('valoresAdultos'); 
+    let adultos = valoresAdultos.split(','); 
+    for(let i = 0; i < adultos.length; i++)
+    {
+        contenido += '<div class="form-group">'+
+        '<h3>Numero de adultos: '+adultos[i]+'</h3>'+
+        '</div>';
+    }
+
+    let valoresMenores = localStorage.getItem('valoresMenores'); 
+    let menores = valoresMenores.split(','); 
+    for(let i = 0; i < menores.length; i++)
+    {
+        contenido += '<div class="form-group">'+
+        '<h3>Numero de niños: '+menores[i]+'</h3>'+
+        '</div>';
+    }
+
+    let valoresEdad = localStorage.getItem('valoresEdad'); 
+    let edades = valoresEdad.split(','); 
+    if(edades > 0)
+    {
+        for(let i = 0; i < edades.length; i++)
+        {
+            contenido += '<div class="form-group">'+
+            '<h3>Edad niño '+(i+1)+': '+edades[i]+'</h3>'+
+            '</div>';
+        }
+    }
+    document.getElementById("nodos").innerHTML = contenido; 
+})();
